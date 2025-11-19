c412522561fa3adfdb14888d618310f5bebca0eb
Natan <xpenatan@gmail.com>
2025-11-18 22:10:11 -0300

    Fix setting lib version

--- lua/lua-teavm/build.gradle.kts
+++ lua/lua-teavm/build.gradle.kts
@@ -44,6 +44,8 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             artifactId = moduleName
+            group = LibExt.groupId
+            version = LibExt.libVersion
             from(components["java"])
         }
     }
